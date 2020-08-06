9e1671d56eebbb3c481dae6c81c6e1d04731bd6e
Krutika-B <krutikabramh@gmail.com>
2020-08-06 23:21:25 +0530

    initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/NewsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
